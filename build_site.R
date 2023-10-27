3165df3340a37377929d2ced57db3cb10481f67c
Amit Lad <amitlad@Amits-MacBook-Pro.local>
2023-10-27 13:52:39 +0100

    formatting updates

--- build_site.R
+++ build_site.R
@@ -3,4 +3,5 @@ library(here)
 source("data", "s3_tour_france", "stage3.R")
 
 rmarkdown::render("s3_tour_france.Rmd", output_file = "_site/index.html")
+rmarkdown::render_supporting_files(from = "www", files_dir = "_site")
   
