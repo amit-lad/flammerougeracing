07ae4e88ba63f1355d5e0b3f1c2cb3fb12e1b091
Amit Lad <amitlad@Amits-MBP-2.home>
2023-12-01 20:25:03 +0000

    restructure

--- build_site.R
+++ build_site.R
@@ -3,5 +3,4 @@ library(here)
 
 
 rmarkdown::render_supporting_files(from = "www", files_dir = "_site")
-rmarkdown::render("s3_tour_france.Rmd", output_file = "_site/index.html")
-    
+rmarkdown::render("s3_tour_france.Rmd", output_file = "_site/s3_France.html")
