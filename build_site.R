4fe20df64192367414a853b70b0e247058e7bed4
Amit Lad <amitlad@Amits-MBP-2.home>
2023-12-01 20:29:04 +0000

    rename

--- build_site.R
+++ build_site.R
@@ -3,4 +3,4 @@ library(here)
 
 
 rmarkdown::render_supporting_files(from = "www", files_dir = "_site")
-rmarkdown::render("s3_tour_france.Rmd", output_file = "_site/s3_France.html")
+rmarkdown::render("s3_tour_france.Rmd", output_file = "_site/s3_tour_france.html")
